3288c0704ecc47755c27d25147ee0b5149242e43
Jason Street <jason.street@learningpeople.co.uk>
2018-12-04 16:05:59 +0000

    added switches

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = "jasons_cook_book"
+app.config["MONGO_URI"] = "mongodb://admin:TLPjason1801@ds123796.mlab.com:23796/jasons_cook_book"
 
+mongo = PyMongo(app)
 
 @app.route("/")
-def hello():
-    return "Hello World"
+@app.route("/get_recipes")
+def get_recipes():
+    return render_template("recipes.html", recipes=mongo.db.recipes.find())
+    
+@app.route("/add_recipe")    
+def add_recipe():
+    return render_template("addrecipe.html")
     
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
