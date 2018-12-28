0b1521e6c1841bd760ec8f1e18bf24484c5069e2
Jason Street <jason.street@learningpeople.co.uk>
2018-12-28 10:34:56 +0000

    commit 28 dec all

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = "jasons_cook_book"
@@ -24,6 +25,12 @@ def insert_recipe():
     recipes = mongo.db.recipes
     recipes.insert_one(request.form.to_dict())
     return redirect(url_for("get_recipes"))
+    
+@app.route("/edit_recipe/<recipe_id>")
+def edit_recipe(recipe_id):
+    the_recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    all_categories = mongo.db.categories.find()
+    return render_template("editrecipe.html", recipe=the_recipe, categories=all_categories)
     
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
